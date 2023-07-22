fa7becb1ef479190e52728bbe39e1bee25f4ba94
NirpendraNathMishra <nirpendranathmishra1947@gmail.com>
2023-07-23 01:49:21 +0530

    used new hooks
    used new hooks

--- src/App.js
+++ src/App.js
@@ -1,41 +1,32 @@
-import React,{Component} from "react";
+import React,{useState,useEffect} from "react";
 import Cardlist from "./Cardlist";
 import Searchbox from "./searchbox";
 import Scroll from "./scroll";
 import "./App.css";
-const state={
-    robots:[],
-    searchfield:""
-}
-
-class  App extends Component  {
-    constructor(){
-        super()
-        this.state= {
-            robots:[],
-            searchfield:""
-        }
-    }
-    onsearchchange=(event)=>{
-        this.setState({searchfield:event.target.value})
+ function App ()  {
+    
+     const onsearchchange=(event)=>{
+        setSearchfield(event.target.value)
         
     }
-    componentDidMount(){
+    const [robots,setRobots]=useState([])
+    const [searchfield,setSearchfield]=useState("")
+    useEffect(()=>{
         fetch("https://jsonplaceholder.typicode.com/users")
         .then(response=>response.json())
-        .then(users=>this.setState({robots:users}))
-    }
-    render(){
-        const filteredrobots=this.state.robots.filter(robots=>{
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        .then(users=>setRobots(users))
+    },[])
+    {
+        const filteredrobots=robots.filter(robots=>{
+            return robots.name.toLowerCase().includes(searchfield.toLowerCase())
         })
-        if (this.state.robots.length===0) {
+        if (robots.length===0) {
             return <h1>Loading</h1>
         } else {
             return (
                 <div id="app">
                 <h1>RoboFriends</h1>
-                <Searchbox searchchange={this.onsearchchange}></Searchbox>
+                <Searchbox searchchange={onsearchchange}></Searchbox>
                 <Scroll>
                 <Cardlist robots={filteredrobots}></Cardlist>
                 </Scroll>
